f032528c4825c4375a8bf847a5eee35f98ec2c5d
inamkhn <inamkhn7786@gmail.com>
2024-02-05 06:34:38 -0800

    add cors

--- api/server.js
+++ api/server.js
@@ -18,11 +18,32 @@ const __dirname = path.resolve();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 // app.use(cors())
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://your-frontend.com"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 5173);
+
+  next();
+});
+
 const corsOptions ={
     origin: 'http://localhost:5173', 
     credentials: true,    
     optionSuccessStatus: 200,
-    
 }
 app.use(cors(corsOptions))
 dotenv.config()
