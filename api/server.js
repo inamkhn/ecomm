a484a29963e75fe61aa7964c4f023219e0270338
inamkhn <inamkhn7786@gmail.com>
2024-02-06 06:16:51 -0800

    server.js file changes

--- api/server.js
+++ api/server.js
@@ -11,10 +11,9 @@ import { connect } from "./config/db.js";
 dotenv.config()
 
 const app = express()
-// const __dirname = path.resolve();
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
-// app.use(cors())
 const corsOptions ={
     origin: 'http://localhost:5173', 
     credentials: true,    
@@ -33,26 +32,11 @@ app.use("/api/v1",userRouter)
 app.use("/api/v1",orderRouter)
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-// })
 
-if (process.env.NODE_ENV === 'production') {
-  const __dirname = path.resolve();
-  // app.use('/uploads', express.static('/var/data/uploads'));
-  app.use(express.static(path.join(__dirname, '/client/build')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  );
-} else {
-  const __dirname = path.resolve();
-  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
 
 
 app.listen(5000,(req,res)=>{
