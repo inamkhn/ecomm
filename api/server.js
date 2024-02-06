172cd6ef28896b750db7f02c213b420a467bda05
inamkhn <inamkhn7786@gmail.com>
2024-02-06 07:39:43 -0800

    new

--- api/server.js
+++ api/server.js
@@ -14,11 +14,11 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
-const corsOptions ={
-    origin: 'http://localhost:5173', 
-    credentials: true,    
-    optionSuccessStatus: 200,
-}
+// const corsOptions ={
+//     origin: 'http://localhost:5173', 
+//     credentials: true,    
+//     optionSuccessStatus: 200,
+// }
 
 app.use(cors())
 dotenv.config()
